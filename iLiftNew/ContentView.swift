33fbfe07a17ebacc4942cb2d8e79bd1d29acda79
Armaan <armaankotecha@gmail.com>
2022-12-11 17:59:41 -0500

    Initial Commit

---
+++ iLiftNew/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iLiftNew
+//
+//  Created by akotecha on 12/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iLiftNew/iLiftNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLiftNewApp.swift
+//  iLiftNew
+//
+//  Created by akotecha on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iLiftNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
